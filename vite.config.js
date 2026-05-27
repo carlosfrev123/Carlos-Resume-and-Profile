d86b223c87fc72b22ce9d36b68572ebd12266540
Carlos Revilla <carlosrevilla123@gmail.com>
2026-05-27 17:39:54 -0400

    update vite base to proper name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Carlos-Resume-and-Portfolio/'
+  base: '/Carlos-Resume-and-Profile/'
 })
